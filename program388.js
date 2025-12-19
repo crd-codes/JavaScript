b50d5ebedc80e7d5b4275d03ff9be1a62f4d9ced
CR <dugadechakradhar@gmail.com>
2025-12-19 11:14:44 +0530

    Added program

--- program388.js
+++ program388.js
@@ -1,1 +1,32 @@
-// checkcmail for code
+class node
+{
+    constructor(no)
+    {
+        this.data = no;
+        this.next = null;
+    }
+}
+
+class SinglyLL
+{
+    constructor()
+    {
+        this.head = null;
+    }
+
+    InsertFirst(no)
+    {}
+
+    InsertLast(no)
+    {}
+
+    InsertAtPos(no, pos)
+    {}
+}
+
+function main()
+{
+
+}
+
+main();
